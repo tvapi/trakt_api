b9619d5f641b762c2418952e6c61f4a30600c92b
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2014-06-28 12:06:56 +0200

    refactor search methods

--- lib/trakt_api/search.rb
+++ lib/trakt_api/search.rb
@@ -1,26 +1,88 @@
 class TraktApi::Search < TraktApi::Base
   # documentation: http://trakt.tv/api-docs/search-episodes
   def episodes(options = {})
-    get('search/episodes').params(options).response
+    episodes_path_with_params(options).get
+  end
+
+  def episodes_url(options = {})
+    episodes_path_with_params(options).url
   end
 
   # documentation: http://trakt.tv/api-docs/search-movies
   def movies(options = {})
-    get('search/movies').params(options).response
+    movies_path_with_params(options).get
+  end
+
+  def movies_url(options = {})
+    movies_path_with_params(options).url
   end
 
   # documentation: http://trakt.tv/api-docs/search-people
   def people(options = {})
-    get('search/people').params(options).response
+    people_path_with_params(options).get
+  end
+
+  def people_url(options = {})
+    people_path_with_params(options).url
   end
 
   # documentation: http://trakt.tv/api-docs/search-shows
   def shows(options = {})
-    get('search/shows').params(options).response
+    shows_path_with_params(options).get
+  end
+
+  def shows_url(options = {})
+    shows_path_with_params(options).url
   end
 
   # documentation: http://trakt.tv/api-docs/search-users
   def users(options = {})
-    get('search/users').params(options).response
+    users_path_with_params(options).get
+  end
+
+  def users_url(options = {})
+    users_path_with_params(options).url
+  end
+
+  private
+
+  def episodes_path_with_params(options)
+    path(episodes_path).params(api_key_options.merge(options))
+  end
+
+  def episodes_path
+    'search/episodes.json/:api_key'
+  end
+
+  def movies_path_with_params(options)
+    path(movies_path).params(api_key_options.merge(options))
+  end
+
+  def movies_path
+    'search/movies.json/:api_key'
+  end
+
+  def people_path_with_params(options)
+    path(people_path).params(api_key_options.merge(options))
+  end
+
+  def people_path
+    'search/people.json/:api_key'
+  end
+
+  def shows_path_with_params(options)
+    path(shows_path).params(api_key_options.merge(options))
+  end
+
+  def shows_path
+    'search/shows.json/:api_key'
+  end
+
+  def users_path_with_params(options)
+    path(users_path).params(api_key_options.merge(options))
+  end
+
+  def users_path
+    'search/users.json/:api_key'
   end
 end
--- spec/functionals/search_spec.rb
+++ spec/functionals/search_spec.rb
@@ -1,85 +1,102 @@
 require 'spec_helper'
 
 describe TraktApi::Search do
-  let(:model) { TraktApi::Search.new(TraktApi::Client.new) }
-  let(:mock_model) { SampleModel.new }
+  let(:client) { TraktApi::Client.new(api_key: '123456789', adapter: :test, adapter_options: faraday_stubs) }
+  let(:model) { client.search }
 
-  before do
-    pending 'FIXME'
+  let(:data_episodes) { File.read('spec/fixtures/search_episodes.json') }
+  let(:data_movies) { File.read('spec/fixtures/search_movies.json') }
+  let(:data_people) { File.read('spec/fixtures/search_people.json') }
+  let(:data_shows) { File.read('spec/fixtures/search_shows.json') }
+  let(:data_users) { File.read('spec/fixtures/search_users.json') }
+
+  let(:faraday_stubs) do
+    Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.get('/search/episodes.json/123456789') { [200, { content_type: 'json' }, data_episodes] }
+      stub.get('/search/movies.json/123456789') { [200, { content_type: 'json' }, data_movies] }
+      stub.get('/search/people.json/123456789') { [200, { content_type: 'json' }, data_people] }
+      stub.get('/search/shows.json/123456789') { [200, { content_type: 'json' }, data_shows] }
+      stub.get('/search/users.json/123456789') { [200, { content_type: 'json' }, data_users] }
+    end
   end
 
   describe '.episodes' do
-    it 'should call get with specific params' do
-      model.instance_variable_set("@method", :get)
-      model.should_receive(:get).with('search/episodes').and_return(mock_model)
-
-      model.episodes(query: 'test')
+    it 'should return Faraday::Response class' do
+      model.episodes.class.should == Faraday::Response
     end
 
-    it 'should call restful_params' do
-      model.should_receive(:params).and_return(mock_model)
+    it 'should return Hash class for body reponse' do
+      model.episodes.body == Hash
+    end
+  end
 
-      model.episodes(query: 'test')
+  describe '.episodes_url' do
+    it 'should return correct url' do
+      model.episodes_url.should == 'http://api.trakt.tv/search/episodes.json/123456789'
     end
   end
 
   describe '.movies' do
-    it 'should call get with specific params' do
-      model.instance_variable_set("@method", :get)
-      model.should_receive(:get).with('search/movies').and_return(mock_model)
-
-      model.movies(query: 'test')
+    it 'should return Faraday::Response class' do
+      model.movies.class.should == Faraday::Response
     end
 
-    it 'should call restful_params' do
-      model.should_receive(:params).and_return(mock_model)
+    it 'should return Hash class for body reponse' do
+      model.movies.body == Hash
+    end
+  end
 
-      model.movies(query: 'test')
+  describe '.movies_url' do
+    it 'should return correct url' do
+      model.movies_url.should == 'http://api.trakt.tv/search/movies.json/123456789'
     end
   end
 
   describe '.people' do
-    it 'should call get with specific params' do
-      model.instance_variable_set("@method", :get)
-      model.should_receive(:get).with('search/people').and_return(mock_model)
-
-      model.people(query: 'test')
+    it 'should return Faraday::Response class' do
+      model.people.class.should == Faraday::Response
     end
 
-    it 'should call restful_params' do
-      model.should_receive(:params).and_return(mock_model)
+    it 'should return Hash class for body reponse' do
+      model.people.body == Hash
+    end
+  end
 
-      model.people(query: 'test')
+  describe '.people_url' do
+    it 'should return correct url' do
+      model.people_url.should == 'http://api.trakt.tv/search/people.json/123456789'
     end
   end
 
   describe '.shows' do
-    it 'should call get with specific params' do
-      model.instance_variable_set("@method", :get)
-      model.should_receive(:get).with('search/shows').and_return(mock_model)
-
-      model.shows(query: 'test')
+    it 'should return Faraday::Response class' do
+      model.shows.class.should == Faraday::Response
     end
 
-    it 'should call restful_params' do
-      model.should_receive(:params).and_return(mock_model)
+    it 'should return Hash class for body reponse' do
+      model.shows.body == Hash
+    end
+  end
 
-      model.shows(query: 'test')
+  describe '.shows_url' do
+    it 'should return correct url' do
+      model.shows_url.should == 'http://api.trakt.tv/search/shows.json/123456789'
     end
   end
 
   describe '.users' do
-    it 'should call get with specific params' do
-      model.instance_variable_set("@method", :get)
-      model.should_receive(:get).with('search/users').and_return(mock_model)
-
-      model.users(query: 'test')
+    it 'should return Faraday::Response class' do
+      model.users.class.should == Faraday::Response
     end
 
-    it 'should call restful_params' do
-      model.should_receive(:params).and_return(mock_model)
+    it 'should return Hash class for body reponse' do
+      model.users.body == Hash
+    end
+  end
 
-      model.users(query: 'test')
+  describe '.users_url' do
+    it 'should return correct url' do
+      model.users_url.should == 'http://api.trakt.tv/search/users.json/123456789'
     end
   end
 end
